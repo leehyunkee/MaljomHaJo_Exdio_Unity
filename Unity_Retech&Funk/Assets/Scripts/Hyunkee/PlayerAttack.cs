b8dce94b224d8e80605d6bdb1eda0e57466593f1
leehyunkee <brdn2925@gmail.com>
2022-11-14 22:32:38 +0900

    11/14 software develope
    근접공격 및 콤보공격 구현
    근접공격 후 쿨타임 적용 및 아이템 획득 시 콤보 횟수 증가 구현(적용은 안함)
    count 이용해서 3회 콤보에서 공격할때마다 -1씩 0이 되면 쿨타임 적용하는 방식으로 구현

---
+++ Unity_Retech&Funk/Assets/Scripts/Hyunkee/PlayerAttack.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private float curTime;
+    public float coolTime = 0.5f;
+    private float ComboCount = 3;
+    public float UpdateComboCount = 3;
+    private float comboCurTime;
+    public float comboCoolTime;
+    public Transform pos;
+    public Vector2 boxSize;
+    private void Update()
+    {
+        if (curTime <= 0 && comboCurTime <= 0) 
+        {
+            if (Input.GetKey(KeyCode.X))
+            {
+                //X 키를 누르면 근접공격이 나감
+                ComboCount -= 1;
+                Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(pos.position, boxSize, 0);
+                foreach (Collider2D collider in collider2Ds)
+                {
+                    //지정한 위치의 범위 내에 적이 있는 상태에서 공격 시 충돌처리
+                    Debug.Log(collider.tag);
+                }
+                curTime = coolTime;
+            }
+            if (ComboCount <= 0)
+            {
+                comboCurTime = comboCoolTime;
+                ComboCount = UpdateComboCount;
+            }
+        }
+        else
+        {
+            curTime -= Time.deltaTime;
+            comboCurTime -= Time.deltaTime;
+        }
+
+    
+    }
+    private void OnDrawGizmos()
+    {
+        //범위 사이즈 확인을 위해 넣어둔 코드
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(pos.position, boxSize);
+    }
+}
