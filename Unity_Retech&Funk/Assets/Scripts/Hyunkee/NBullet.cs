90103f92b33155a88250db4d4ee9ff4b4a3ede71
leehyunkee <brdn2925@gmail.com>
2022-11-20 22:25:01 +0900

    11/16
    패링 구현 시도했지만 실패
    탄막에 피격 시 데미지(생명 닳기) 구현중

---
+++ Unity_Retech&Funk/Assets/Scripts/Hyunkee/NBullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NBullet : MonoBehaviour
+{
+
+    public GameObject bullet; // 총알 오브젝트
+    Vector2 pos;
+
+    void Start()
+    {
+        StartCoroutine("Shot"); //반복해서 함수 수행
+        pos = gameObject.transform.position;
+        //pos = this.gameObject.transform.localPosition; //몬스터의 현재 위치
+    }
+
+
+    IEnumerator Shot()
+    {
+
+        GameObject temp = Instantiate(bullet); // 총알 복제
+        temp.transform.position = pos; // 몬스터의 현재 위치에서 총알 복제
+        yield return new WaitForSeconds(0.3f); // 3초 간격으로 복제
+        StartCoroutine("Shot"); //반복해서 함수 수행
+
+    }
+}
