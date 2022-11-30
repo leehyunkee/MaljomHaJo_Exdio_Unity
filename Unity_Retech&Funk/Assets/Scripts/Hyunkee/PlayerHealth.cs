000fb4ffbfb8467bfa27fba38f6601ffb3b908b1
leehyunkee <brdn2925@gmail.com>
2022-11-30 21:26:11 +0900

    11/30
    충돌처리 관련 오류 모두 수정 완료. 원거리, 근거리 공격 관련 데미지, 충돌처리 작업 완료 및 피격 시 무적상태(깜빡임) 적용 완료

--- Unity_Retech&Funk/Assets/Scripts/Hyunkee/PlayerHealth.cs
+++ Unity_Retech&Funk/Assets/Scripts/Hyunkee/PlayerHealth.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public bool isHurt;
-    SpriteRenderer sr;
+    public bool isHurt = false;
+    public SpriteRenderer sr;
     Color halfA = new Color(1, 1, 1, 0.5f);
     Color fullA = new Color(1, 1, 1, 1);
 
@@ -18,14 +18,36 @@ public class PlayerHealth : MonoBehaviour
     void Start()
     {
         nowHp = Hp;
-    }
-    public void TakeDamage(int damage)
-    {
-        nowHp -= damage;
-        Debug.Log("Damage");
 
     }
     
+
+    public Animator anim;
+    public GameObject obj;
+   
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isHurt == false && collision.gameObject.tag == ("Enemy"))
+        {
+            //collision이 충돌체의 정보를 다 갖고있음. => 부딫힌 gameobject 자체를 바로 없앨 수 있음
+
+            Destroy(collision.gameObject);
+            Debug.Log("아야");
+            Hurt();
+            
+        }
+        else if (isHurt == false && collision.gameObject.tag == ("sdEnemy"))
+        {
+            //collision이 충돌체의 정보를 다 갖고있음. => 부딫힌 gameobject 자체를 바로 없앨 수 있음
+
+            
+            Debug.Log("근접아야");
+            Hurt();
+
+        }
+
+    }
     public void Hurt()
     {
 
@@ -34,6 +56,7 @@ public class PlayerHealth : MonoBehaviour
         {
             isHurt = true;
             nowHp -= 1;
+            Debug.Log("데미지 들어감");
             if (nowHp <= 0)
             {
                 Invoke("freeze", 1);
@@ -49,7 +72,7 @@ public class PlayerHealth : MonoBehaviour
     }
     IEnumerator HurtRoutine()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(1f);
         Debug.Log("쉬어가기");
         isHurt = false;
     }
