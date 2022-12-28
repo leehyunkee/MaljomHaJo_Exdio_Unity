02217a7cf1c33db3f01ef4937e81f9b2b6232022
leehyunkee <brdn2925@gmail.com>
2022-12-28 20:34:31 +0900

    12/28
    패링을 구현하려고 시도함
    오류발생
    -데미지, 모두 인식하지만 패링탄을 근접공격으로 친다 라는 부분이 인식이 안되는중.
    오류 해결 아이디어
    -타 스크립트 변수, 함수 자유자재로 불러오기 공부하기
    -모든 부분을 on trigger enter 2d를 이용해 동일하게 구현해볼 예정
    -메모장에 하나하나 다시 작성해 처음부터 하나씩 구현 및 현재 스크립트 정리 및 통일 작업 필요 예상

--- Unity_Retech&Funk/Assets/Scripts/Hyunkee/PlayerAttack.cs
+++ Unity_Retech&Funk/Assets/Scripts/Hyunkee/PlayerAttack.cs
@@ -7,8 +7,11 @@ public class PlayerAttack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+    public GameObject bullet;
+    public Transform parrypos;
+
 
     private float curTime;
     public float coolTime = 0.5f;
@@ -18,19 +21,49 @@ public class PlayerAttack : MonoBehaviour
     public float comboCoolTime;
     public Transform pos;
     public Vector2 boxSize;
+    public bool isParry = false;
     private void Update()
     {
-        if (curTime <= 0 && comboCurTime <= 0) 
+        
+
+
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (curTime <= 0 && comboCurTime <= 0)
         {
             if (Input.GetMouseButtonDown(0))
             {
-                //X 키를 누르면 근접공격이 나감
+                if (collision.gameObject.tag == "parry")
+                {
+                    IsParryTrue();
+                }
+                //마우스 왼쪽 키를 누르면 근접공격이 나감
                 ComboCount -= 1;
                 Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(pos.position, boxSize, 0);
                 foreach (Collider2D collider in collider2Ds)
                 {
-                    //지정한 위치의 범위 내에 적이 있는 상태에서 공격 시 충돌처리
-                    Debug.Log("에라이 이놈");
+                    Vector2 len = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+                    float z = Mathf.Atan2(len.y, len.x) * Mathf.Rad2Deg;
+                    transform.rotation = Quaternion.Euler(0, 0, z);
+                    if (comboCurTime <= 0)
+                    {
+                        //마우스 왼쪽 버튼 누를 때
+                        if (isParry == true)
+                        {
+                            Instantiate(bullet, parrypos.position, transform.rotation);
+                            IsParryFalse();
+                        }
+                        else
+                        {
+
+                            //지정한 위치의 범위 내에 적이 있는 상태에서 공격 시 충돌처리
+                            Debug.Log("에라이 이놈");
+                        }
+                    }
+                    comboCurTime -= Time.deltaTime;
+
+
                 }
                 curTime = coolTime;
             }
@@ -45,13 +78,20 @@ public class PlayerAttack : MonoBehaviour
             curTime -= Time.deltaTime;
             comboCurTime -= Time.deltaTime;
         }
-
-    
+        
     }
     private void OnDrawGizmos()
     {
         //범위 사이즈 확인을 위해 넣어둔 코드
         Gizmos.color = Color.blue;
         Gizmos.DrawWireCube(pos.position, boxSize);
+    }
+    public void IsParryTrue()
+    {
+        isParry = true;
+    }
+    public void IsParryFalse()
+    {
+        isParry = false;
     }
 }
--- Unity_Retech&Funk/Assets/Scripts/Hyunkee/PlayerParry.cs
+++ Unity_Retech&Funk/Assets/Scripts/Hyunkee/PlayerParry.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +8,12 @@ public class PlayerParry : MonoBehaviour
     public Transform pos;
     public float cooltime;
     private float curtime;
+    PlayerAttack playerAttack;
+    private void Awake()
+    {
+        playerAttack = GameObject.Find("Player").GetComponent<PlayerAttack>();
+        //특정 스크립트를 불러오기 위한 코드
+    }
 
     private void Update()
     {
@@ -17,12 +23,14 @@ public class PlayerParry : MonoBehaviour
         if (curtime <= 0)
         {
             //마우스 왼쪽 버튼 누를 때
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0) && playerAttack.isParry == true )//코드 내의 변수를 .을 이용해 가져옴
             {
-                Instantiate(bullet, pos.position, transform.rotation);
+                Instantiate(bullet, parrypos.position, transform.rotation);
+                playerAttack.IsParryFalse();//스크립트 내의 함수 가져오기
             }
             curtime = cooltime;
         }
         curtime -= Time.deltaTime;
+
     }
-}
+}*/
