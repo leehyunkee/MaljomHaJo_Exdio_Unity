04e265a9ef464e932e6341bb0479c47db1a537f9
eunji1206 <kimeunji031206@gmail.com>
2023-02-02 21:00:52 +0900

    0202
    일반탄 중간중간에 패링탄이 나오도록 구현중

---
+++ Unity_Retech&Funk/Assets/Scripts/NormalMonster/shot/bulletshot.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulletshot : MonoBehaviour
+{
+
+    public GameObject bullet1; // 총알 오브젝트
+    public GameObject bullet2;
+    Vector2 pos;
+
+    void Start()
+    {
+        StartCoroutine("Shot"); //반복해서 함수 수행
+        StartCoroutine("Shot2");
+        //pos = this.gameObject.transform.localPosition; //몬스터의 현재 위치
+    }
+
+
+    IEnumerator Shot()
+    {
+
+        GameObject temp = Instantiate(bullet1); // 총알 복제
+        pos = gameObject.transform.position;
+        temp.transform.position = pos; // 몬스터의 현재 위치에서 총알 복제
+        yield return new WaitForSeconds(0.3f); // 3초 간격으로 복제
+        StartCoroutine("Shot"); //반복해서 함수 수행
+
+    }
+
+    IEnumerator Shot2()
+    {
+
+        GameObject temp = Instantiate(bullet2); // 총알 복제
+        pos = gameObject.transform.position;
+        temp.transform.position = pos; // 몬스터의 현재 위치에서 총알 복제
+        yield return new WaitForSeconds(5.0f); // 3초 간격으로 복제
+        StartCoroutine("Shot2"); //반복해서 함수 수행
+
+    }
+}
