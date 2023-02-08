dc969e658d7303594fdafc897145b829d9c972c8
eunji1206 <kimeunji031206@gmail.com>
2023-02-08 22:22:09 +0900

    0208
    일반 탄 사이사이에 패링탄이 나오게 끔 구현 하기 위해 구글링
    일반 탄 사이사이에 패링탄이 나오게 끔 구현 하기 위해 코드 수정

--- Unity_Retech&Funk/Assets/Scripts/NormalMonster/shot/bulletshot.cs
+++ Unity_Retech&Funk/Assets/Scripts/NormalMonster/shot/bulletshot.cs
@@ -23,7 +23,7 @@ public class Bulletshot : MonoBehaviour
         GameObject temp = Instantiate(bullet1); // 총알 복제
         pos = gameObject.transform.position;
         temp.transform.position = pos; // 몬스터의 현재 위치에서 총알 복제
-        yield return new WaitForSeconds(0.3f); // 3초 간격으로 복제
+        yield return new WaitForSecondsRealtime(0.3f); // 3초 간격으로 복제
         StartCoroutine("Shot"); //반복해서 함수 수행
 
     }
@@ -34,7 +34,7 @@ public class Bulletshot : MonoBehaviour
         GameObject temp = Instantiate(bullet2); // 총알 복제
         pos = gameObject.transform.position;
         temp.transform.position = pos; // 몬스터의 현재 위치에서 총알 복제
-        yield return new WaitForSeconds(5.0f); // 3초 간격으로 복제
+        yield return new WaitForSecondsRealtime(1.0f); // 3초 간격으로 복제
         StartCoroutine("Shot2"); //반복해서 함수 수행
 
     }
