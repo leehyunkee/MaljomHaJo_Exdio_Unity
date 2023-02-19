b21b5270f2acce8b364fa60e76cd25a706e3648c
leehyunkee <brdn2925@gmail.com>
2023-02-19 20:26:42 +0900

    0219
    몬스터 충돌 상호작용 및 스크립트 정리 완료

---
+++ Unity_Retech&Funk/Assets/Scripts/Hyunkee/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == ("PlayerBullet"))
+        {
+            //collision이 충돌체의 정보를 다 갖고있음. => 부딫힌 gameobject 자체를 바로 없앨 수 있음
+
+            Destroy(collision.gameObject);
+            Debug.Log("적 사망");
+            Dead();
+            
+
+        }
+       
+
+    }
+    void Dead()
+    {
+        Destroy(gameObject);
+    }
+}
